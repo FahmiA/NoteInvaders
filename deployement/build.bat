ad5280ccabba990129dc9526392ffd0158c7a501
Fahmi Abdulhamid <fahabd@coretech.co.nz>
2013-07-01 16:51:31 +1200

    Added Linux build script and updated Windows build script.

--- deployement/build.bat
+++ deployement/build.bat
@@ -1,3 +1,4 @@
 python C:\Software\pyinstaller\pyinstaller.py --upx-dir C:\Software\upx309w NoteWars.spec
-cp -r ../media dist/
+cp -r ../media dist/media
 cp ../highscores.csv dist/
+cp ../README.md dist/readme.txt
---
+++ deployement/build.sh
@@ -0,0 +1,4 @@
+python ~/pyinstaller/pyinstaller.py NoteWars.spec
+cp -r ../media dist/media
+cp ../highscores.csv dist/
+cp ../README.md dist/readme.txt
